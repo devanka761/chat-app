a422e533e6827ee156b74d5c8039a7fcf4534180
Devanka <90453179+devanka761@users.noreply.github.com>
2021-12-09 13:34:09 +0700

    Update config.js

--- data/config/config.js
+++ data/config/config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyB8SPIHdlDqj-c3_1qHEk0PEOibm9tYYT8",
-    authDomain: "messaging761.firebaseapp.com",
-    databaseURL: "https://messaging761-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "messaging761",
-    storageBucket: "messaging761.appspot.com",
-    messagingSenderId: "766173576790",
-    appId: "1:766173576790:web:1a3905fc8cb7d0059045f7"
+    apiKey: "GANTI DENGAN PROJECT KALIAN",
+    authDomain: "GANTI DENGAN PROJECT KALIAN",
+    databaseURL: "GANTI DENGAN PROJECT KALIAN",
+    projectId: "GANTI DENGAN PROJECT KALIAN",
+    storageBucket: "GANTI DENGAN PROJECT KALIAN",
+    messagingSenderId: "GANTI DENGAN PROJECT KALIAN",
+    appId: "GANTI DENGAN PROJECT KALIAN"
 };
 const app = firebase.initializeApp(firebaseConfig);
